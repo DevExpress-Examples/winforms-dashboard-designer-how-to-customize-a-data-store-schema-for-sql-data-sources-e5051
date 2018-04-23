c91f6e86b7abb0f3d9bb6aa35f62f87168343f09
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Dashboard_CustomSchemaProvider/Form1.cs
@@ -0,0 +1,53 @@
+﻿using DevExpress.XtraBars.Ribbon;
+using DevExpress.DashboardCommon;
+using DevExpress.DataAccess.ConnectionParameters;
+using DevExpress.DataAccess;
+using DevExpress.Xpo.DB;
+
+namespace Dashboard_CustomSchemaProvider {
+    public partial class Form1 : RibbonForm {
+        public Form1() {
+            InitializeComponent();
+            dashboardDesigner1.CreateRibbon();
+
+            // Specifies a custom schema provider.
+            dashboardDesigner1.CustomDBSchemaProvider = new CustomDBSchemaProvider();
+
+            // Loads a dashboard from an XML file.
+            dashboardDesigner1.LoadDashboard(@"..\..\Data\dashboard.xml");
+        }
+
+        // Creates a new class that defines a custom data store schema by implementing the 
+        // IDBSchemaProvider interface.
+        class CustomDBSchemaProvider : IDBSchemaProvider {
+            public DBSchema GetSchema(DataConnection dataConnection, string[] tableList) {
+                var cp = dataConnection.ConnectionParameters as Access97ConnectionParameters;
+                if (cp != null && cp.FileName == @"..\..\Data\nwind.mdb") {
+                    return GetSchema(dataConnection);
+                }
+                else
+                    return new DBSchemaProvider().GetSchema(dataConnection, tableList);
+            }
+            public DBSchema GetSchema(DataConnection dataConnection) {
+                var cp = dataConnection.ConnectionParameters as Access97ConnectionParameters;
+                if (cp != null && cp.FileName == @"..\..\Data\nwind.mdb") {
+
+                    // Adds two tables with required columns to a data store schema.
+                    DBTable[] tables = new DBTable[2];
+                    tables[0] = new DBTable("Categories");
+                    tables[0].AddColumn(new DBColumnWithAlias("CategoryName", "Category"));
+                    tables[0].AddColumn(new DBColumnWithAlias("CategoryID", "Category ID"));
+
+                    tables[1] = new DBTableWithAlias("Products", "Product list");
+                    tables[1].AddColumn(new DBColumnWithAlias("ProductName", "Product"));
+                    tables[1].AddColumn(new DBColumnWithAlias("CategoryID", "Category ID"));
+
+                    DBTable[] views = new DBTable[0];
+                    return new DBSchema(tables, views);
+                }
+                else
+                    return new DBSchemaProvider().GetSchema(dataConnection);
+            }
+        }
+    }
+}
---
+++ VB/Dashboard_CustomSchemaProvider/Form1.vb
@@ -0,0 +1,60 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.XtraBars.Ribbon
+Imports DevExpress.DashboardCommon
+Imports DevExpress.DataAccess.ConnectionParameters
+Imports DevExpress.DataAccess
+Imports DevExpress.Xpo.DB
+
+Namespace Dashboard_CustomSchemaProvider
+	Partial Public Class Form1
+		Inherits RibbonForm
+		Public Sub New()
+			InitializeComponent()
+			dashboardDesigner1.CreateRibbon()
+
+			' Specifies a custom schema provider.
+			dashboardDesigner1.CustomDBSchemaProvider = New CustomDBSchemaProvider()
+
+			' Loads a dashboard from an XML file.
+			dashboardDesigner1.LoadDashboard("..\..\Data\dashboard.xml")
+		End Sub
+
+		' Creates a new class that defines a custom data store schema by implementing the 
+		' IDBSchemaProvider interface.
+		Private Class CustomDBSchemaProvider
+            Implements IDBSchemaProvider
+            Public Function GetSchema(ByVal dataConnection As DataConnection, _
+                                      ByVal tableList() As String) As DBSchema _
+                                  Implements IDBSchemaProvider.GetSchema
+                Dim cp = TryCast(dataConnection.ConnectionParameters, Access97ConnectionParameters)
+                If cp IsNot Nothing AndAlso cp.FileName = "..\..\Data\nwind.mdb" Then
+                    Return GetSchema(dataConnection)
+                Else
+                    Return New DBSchemaProvider().GetSchema(dataConnection, tableList)
+                End If
+            End Function
+            Public Function GetSchema(ByVal dataConnection As DataConnection) As DBSchema _
+                Implements IDBSchemaProvider.GetSchema
+                Dim cp = TryCast(dataConnection.ConnectionParameters, Access97ConnectionParameters)
+                If cp IsNot Nothing AndAlso cp.FileName = "..\..\Data\nwind.mdb" Then
+
+                    ' Adds two tables with required columns to a data store schema.
+                    Dim tables(1) As DBTable
+                    tables(0) = New DBTable("Categories")
+                    tables(0).AddColumn(New DBColumnWithAlias("CategoryName", "Category"))
+                    tables(0).AddColumn(New DBColumnWithAlias("CategoryID", "Category ID"))
+
+                    tables(1) = New DBTableWithAlias("Products", "Product list")
+                    tables(1).AddColumn(New DBColumnWithAlias("ProductName", "Product"))
+                    tables(1).AddColumn(New DBColumnWithAlias("CategoryID", "Category ID"))
+
+                    Dim views(-1) As DBTable
+                    Return New DBSchema(tables, views)
+                Else
+                    Return New DBSchemaProvider().GetSchema(dataConnection)
+                End If
+            End Function
+        End Class
+	End Class
+End Namespace
