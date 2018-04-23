a3ee5c68e2a7225aff9d02615d976ef14e4007aa
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Dashboard_CustomSchemaProvider/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Specialized;
+using DevExpress.DataAccess.ConnectionParameters;
+using DevExpress.DataAccess.Sql;
+using DevExpress.Xpo.DB;
+using DevExpress.XtraBars.Ribbon;
+
+namespace Dashboard_CustomSchemaProvider {
+    public partial class Form1 : RibbonForm {
+        public Form1() {
+            InitializeComponent();
+            dashboardDesigner1.CreateRibbon();
+
+            // Specifies a custom schema provider.
+            dashboardDesigner1.CustomDBSchemaProvider = new CustomDBSchemaProvider();
+
+            // Loads a dashboard from an XML file.
+            dashboardDesigner1.LoadDashboard(@"..\..\Data\dashboard.xml");
+        }
+
+        // Creates a new class that defines a custom data store schema by implementing the 
+        // IDBSchemaProvider interface.
+        class CustomDBSchemaProvider : IDBSchemaProvider {
+            public DBSchema GetSchema(SqlDataConnection connection, SchemaLoadingMode schemaLoadingMode) {
+                var cp = connection.ConnectionParameters as Access97ConnectionParameters;
+                if (cp != null && cp.FileName == @"..\..\Data\nwind.mdb") {
+
+                    DBTable[] tables;
+                    if (schemaLoadingMode.HasFlag(SchemaLoadingMode.TablesAndViews)) {
+                        // Creates two tables with required columns to be added to a data store schema.
+                        tables = new DBTable[2];
+                        DBTable categoriesTable = new DBTable("Categories");
+                        DBColumn categoryNameColumn1 = new DBColumn { Name = "CategoryName" };
+                        categoriesTable.AddColumn(categoryNameColumn1);
+                        DBColumn categoryIdColumn1 = new DBColumn { Name = "CategoryID" };
+                        categoriesTable.AddColumn(categoryIdColumn1);
+                        tables[0] = categoriesTable;
+
+                        DBTable productsTable = new DBTable("Products");
+                        DBColumn productNameColumn2 = new DBColumn { Name = "ProductName" };
+                        productsTable.AddColumn(productNameColumn2);
+                        DBColumn categoryIdColumn2 = new DBColumn { Name = "CategoryID" };
+                        productsTable.AddColumn(categoryIdColumn2);
+                        tables[1] = productsTable;
+
+                        // Creates a foreign key for the 'Products' table that points to the 'CategoryID' 
+                        // column in the 'Categories' table.
+                        DBForeignKey foreignKey = new DBForeignKey(
+                            new[] { categoryIdColumn2 },
+                            categoriesTable.Name,
+                            CustomDBSchemaProvider.CreatePrimaryKeys(categoryIdColumn1.Name));
+                        productsTable.ForeignKeys.Add(foreignKey);
+                    }
+                    else
+                        tables = new DBTable[0];
+
+                    DBTable[] views = new DBTable[0];
+                    return new DBSchema(tables, views);
+                }
+                else
+                    return connection.GetDBSchema();
+            }
+
+            public void LoadColumns(SqlDataConnection connection, params DBTable[] tables) {
+                connection.LoadDBColumns(tables);
+            }
+
+            public static StringCollection CreatePrimaryKeys(params string[] names) {
+                StringCollection collection = new StringCollection();
+                collection.AddRange(names);
+                return collection;
+            }
+        }
+    }
+}
---
+++ VB/Dashboard_CustomSchemaProvider/Form1.vb
@@ -0,0 +1,80 @@
+﻿Imports System.Collections.Specialized
+Imports DevExpress.DataAccess.ConnectionParameters
+Imports DevExpress.DataAccess.Sql
+Imports DevExpress.Xpo.DB
+Imports DevExpress.XtraBars.Ribbon
+
+Namespace Dashboard_CustomSchemaProvider
+    Partial Public Class Form1
+        Inherits RibbonForm
+
+        Public Sub New()
+            InitializeComponent()
+            dashboardDesigner1.CreateRibbon()
+
+            ' Specifies a custom schema provider.
+            dashboardDesigner1.CustomDBSchemaProvider = New CustomDBSchemaProvider()
+
+            ' Loads a dashboard from an XML file.
+            dashboardDesigner1.LoadDashboard("..\..\Data\dashboard.xml")
+        End Sub
+
+        ' Creates a new class that defines a custom data store schema by implementing the 
+        ' IDBSchemaProvider interface.
+        Private Class CustomDBSchemaProvider
+            Implements IDBSchemaProvider
+
+            Public Function GetSchema(ByVal connection As SqlDataConnection, _
+                                      ByVal schemaLoadingMode As SchemaLoadingMode) As DBSchema _
+                                  Implements IDBSchemaProvider.GetSchema
+                Dim cp = TryCast(connection.ConnectionParameters, Access97ConnectionParameters)
+                If cp IsNot Nothing AndAlso cp.FileName = "..\..\Data\nwind.mdb" Then
+
+                    Dim tables() As DBTable
+                    If schemaLoadingMode.HasFlag(schemaLoadingMode.TablesAndViews) Then
+                        ' Creates two tables with required columns to be added to a data store schema.
+                        tables = New DBTable(1) {}
+                        Dim categoriesTable As New DBTable("Categories")
+                        Dim categoryNameColumn1 As DBColumn = New DBColumn With {.Name = "CategoryName"}
+                        categoriesTable.AddColumn(categoryNameColumn1)
+                        Dim categoryIdColumn1 As DBColumn = New DBColumn With {.Name = "CategoryID"}
+                        categoriesTable.AddColumn(categoryIdColumn1)
+                        tables(0) = categoriesTable
+
+                        Dim productsTable As New DBTable("Products")
+                        Dim productNameColumn2 As DBColumn = New DBColumn With {.Name = "ProductName"}
+                        productsTable.AddColumn(productNameColumn2)
+                        Dim categoryIdColumn2 As DBColumn = New DBColumn With {.Name = "CategoryID"}
+                        productsTable.AddColumn(categoryIdColumn2)
+                        tables(1) = productsTable
+
+                        ' Creates a foreign key for the 'Products' table that points to the 'CategoryID' 
+                        ' column in the 'Categories' table.
+                        Dim foreignKey As New DBForeignKey({categoryIdColumn2}, categoriesTable.Name,
+                                                           CustomDBSchemaProvider.CreatePrimaryKeys(categoryIdColumn1.Name))
+                        productsTable.ForeignKeys.Add(foreignKey)
+                    Else
+                        tables = New DBTable() {}
+                    End If
+
+                    Dim views(-1) As DBTable
+                    Return New DBSchema(tables, views)
+                Else
+                    Return connection.GetDBSchema()
+                End If
+            End Function
+
+            Public Sub LoadColumns(ByVal connection As SqlDataConnection,
+                                   ByVal ParamArray tables() As DBTable) _
+                               Implements IDBSchemaProvider.LoadColumns
+                connection.LoadDBColumns(tables)
+            End Sub
+
+            Public Shared Function CreatePrimaryKeys(ByVal ParamArray names() As String) As StringCollection 
+                Dim collection As New StringCollection()
+                collection.AddRange(names)
+                Return collection
+            End Function
+        End Class
+    End Class
+End Namespace
