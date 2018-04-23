0ec61ded69c7776269a7fb95cb5e5a06a7efc265
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# WinForms Dashboard Designer - How to customize a data store schema for SQL data sources
+
+
+<p>This example demonstrates how to customize a data store schema for a dashboard data source that uses a connection to the Northwind database.</p>
+<p>In this example, the <a href="https://documentation.devexpress.com/CoreLibraries/DevExpress.DataAccess.Sql.IDBSchemaProviderEx.class">IDBSchemaProviderEx</a> interface is implemented by a class that defines a custom data store schema containing two related tables. </p>
+<p>To see the result, add a new query or edit the existing query.</p>
+
+<br/>
+
+
---
+++ VB/Dashboard_CustomSchemaProvider/Form1.vb
@@ -0,0 +1,50 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.XtraBars.Ribbon
+Imports DevExpress.DataAccess.ConnectionParameters
+Imports DevExpress.DataAccess
+Imports DevExpress.Xpo.DB
+Imports DevExpress.DataAccess.Sql
+
+Namespace Dashboard_CustomSchemaProvider
+    Partial Public Class Form1
+        Inherits RibbonForm
+        Public Sub New()
+            InitializeComponent()
+            dashboardDesigner1.CreateRibbon()
+
+            ' Specifies a custom schema provider.
+            dashboardDesigner1.CustomDBSchemaProvider = New CustomDBSchemaProvider()
+
+            ' Loads a dashboard from an XML file.
+            dashboardDesigner1.LoadDashboard("..\..\Data\dashboard.xml")
+        End Sub
+
+        ' Creates a new class that defines a custom data store schema by implementing the 
+        ' IDBSchemaProvider interface.
+        Private Class CustomDBSchemaProvider
+            Implements IDBSchemaProvider
+            Public Function GetSchema(ByVal dataConnection As SqlDataConnection) As DBSchema _
+                                  Implements IDBSchemaProvider.GetSchema
+                Dim cp = TryCast(dataConnection.ConnectionParameters, Access97ConnectionParameters)
+                If cp IsNot Nothing AndAlso cp.FileName = "..\..\Data\nwind.mdb" Then
+
+                    ' Adds two tables with required columns to a data store schema.
+                    Dim tables(1) As DBTable
+                    tables(0) = New DBTable("Categories")
+                    tables(0).AddColumn(New DBColumnWithAlias("CategoryName", "Category"))
+                    tables(0).AddColumn(New DBColumnWithAlias("CategoryID", "Category ID"))
+
+                    tables(1) = New DBTableWithAlias("Products", "Product list")
+                    tables(1).AddColumn(New DBColumnWithAlias("ProductName", "Product"))
+                    tables(1).AddColumn(New DBColumnWithAlias("CategoryID", "Category ID"))
+
+                    Dim views(-1) As DBTable
+                    Return New DBSchema(tables, views)
+                Else
+                    Return dataConnection.GetDBSchema()
+                End If
+            End Function
+        End Class
+    End Class
+End Namespace
